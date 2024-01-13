9397983e3165ddc1c8c36f90bd06f096e9f5aff0
jibjoub <jean-baptiste.despujol@epita.fr>
2024-01-13 13:11:52 +0100

    Make modification of variables for Java and update dependency to work on AS Hedgehog

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
