791137d1b35e98fc14c411195f5f291e765befa3
jibjoub <jean-baptiste.despujol@epita.fr>
2024-01-16 19:06:16 +0100

    Display all the fields from the CountryUi in the DetailScreen

--- presentation/src/main/java/com/example/presentation/view/DetailScreen.kt
+++ presentation/src/main/java/com/example/presentation/view/DetailScreen.kt
@@ -1,11 +1,21 @@
 package com.example.presentation.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.common.models.DataState
 import com.example.presentation.model.CountryUi
 import com.example.presentation.viewmodel.DetailViewModel
@@ -20,7 +30,7 @@ fun DetailScreen(id: String) {
     detailViewModel.getDetailById(id)
     Surface(color = MaterialTheme.colorScheme.background) {
         when (val state = uiState) {
-            is DataState.Success -> Text(text = state.data.commonName)
+            is DataState.Success -> CountryDetails(countryUi = state.data)
             is DataState.Loading ->
                 Loading()
 
@@ -29,4 +39,72 @@ fun DetailScreen(id: String) {
             }
         }
     }
+}
+
+@Composable
+fun CountryDetails(countryUi: CountryUi) {
+    Column(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+    ) {
+        FlagImage(flagUrl = countryUi.flagUrl)
+
+        // Common Name
+        Text(
+            text = countryUi.commonName,
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+
+        // Official Name
+        Text(
+            text = countryUi.officialName,
+            fontSize = 18.sp,
+            color = Color.Gray,
+            modifier = Modifier.padding(vertical = 4.dp),
+        )
+
+        // Continents
+        Text(
+            text = "Continents: ${countryUi.continents}",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 4.dp),
+        )
+
+        // Capitals
+        Text(
+            text = "Capitals: ${countryUi.capitals}",
+            fontSize = 18.sp,
+            modifier = Modifier.padding(vertical = 4.dp),
+        )
+
+        // Population
+        Text(
+            text = "Population: ${countryUi.population}",
+            fontSize = 18.sp,
+            modifier = Modifier.padding(vertical = 4.dp),
+        )
+
+        // Flag Description
+        Text(
+            text = countryUi.flagDescription,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(vertical = 8.dp),
+        )
+    }
+}
+
+@Composable
+fun FlagImage(flagUrl: String) {
+    AsyncImage(
+        model = flagUrl,
+        contentDescription = "Flag",
+        modifier =
+            Modifier
+                .size(256.dp),
+    )
 }
